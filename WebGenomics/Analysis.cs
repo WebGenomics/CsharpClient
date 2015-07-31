ff28618b0d5693bcffac8de5ac5e2d8893183c35
Mohammad Hosseini <theappbanq@gmail.com>
2015-07-31 22:08:13 +0300

    Init

---
+++ WebGenomics/Analysis.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace WebGenomics
+{
+    [DataContract]
+    public class Analysis
+    {
+        [DataMember]
+        private readonly string[] StrongestGenes;
+        [DataMember]
+        private readonly Dictionary<string, double> FullGenome;
+        [DataMember]
+        private readonly string[] Topics;
+        [DataMember]
+        private readonly string url;
+        [DataMember]
+        private readonly string LanguageCode;
+        [DataMember]
+        private readonly string LanguageFullName;
+        [DataMember]
+        private readonly bool Pornography;
+        [DataMember]
+        private readonly bool OffensiveLanguage;
+        public Analysis(string url, string[] genes, string[] topics, Dictionary<string, double> genome, string languageCode, string languageFullName, bool porn, bool offensiveLan)
+        {
+            this.StrongestGenes = genes;
+            this.Topics = topics;
+            this.FullGenome = genome;
+            this.url = url;
+            this.LanguageCode = languageCode;
+            this.LanguageFullName = languageFullName;
+            this.Pornography = porn;
+            this.OffensiveLanguage = offensiveLan;
+        }
+    }
+}
---
+++ WebGenomics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebGenomics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var genomics = GetResult("facebook.com");
+            Console.ReadKey();
+        }
+
+        internal static Analysis GetResult(string uri)
+        {
+            Analysis result = null;
+            try
+            {
+                var url = "http://websitegenomics.cloudapp.net/Classify/?uri=" + uri;
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Encoding = System.Text.UTF8Encoding.UTF8;
+                    wc.Headers["Accept"] = "application/json";
+                    wc.Credentials = new NetworkCredential("demo", "demo");
+                    var jsonString = wc.DownloadString(url);
+                    Console.WriteLine("Result: " + jsonString);
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<Analysis>(jsonString);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                result = null;
+            }
+            return result;
+        }
+    }
+}
